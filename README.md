29bc64339cfc8bb334e3880c7097f6967c2fbe54
Antoine Rebillard <antoine.rebillard@pm.me>
2022-06-20 23:56:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Reponsive dashboard with light and dark theme, using HTML, CSS and Javascript
+
+This is a training project for training HTML, CSS and JS.
+
+Credits to (EGATOR([https://www.youtube.com/c/EGATORTUTORIALS/featured]) for this nice exercice.
+
+## How to run the website ?
+
+Just download the repo and open the file `src/index.html` in a web browser.
---
+++ src/orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'foldable mini drone',
+        productNumber: '85412',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36599',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Drone with camera',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'delivered'
+    }
+]
